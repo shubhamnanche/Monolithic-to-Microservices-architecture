aa6a86055841e28f5c83946569dd5d0a0a10ff58
Shubham Nanche <73213552+shubhamnanche@users.noreply.github.com>
2024-12-11 15:31:02 +0530

    Initial Monolithic architecture

---
+++ src/main/java/com/ssk/quizapp/controller/QuizController.java
@@ -0,0 +1,34 @@
+package com.ssk.quizapp.controller;
+
+import com.ssk.quizapp.model.QuestionWrapper;
+import com.ssk.quizapp.model.Response;
+import com.ssk.quizapp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
+        return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable int id) {
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses) {
+        return quizService.calculateResult(id, responses);
+    }
+
+}
---
+++ src/main/java/com/ssk/quizapp/model/Question.java
@@ -0,0 +1,23 @@
+package com.ssk.quizapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+//@Table(name="question")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String category;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String difficultyLevel;
+
+}
